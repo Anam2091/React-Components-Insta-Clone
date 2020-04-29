d163a791f1c032c08af847f41c9be0c13c0a738e
Anam2091 <56008677+Anam2091@users.noreply.github.com>
2020-04-28 23:38:23 -0600

    Props

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,13 +3,21 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
-
+import {useState} from "react"
+import dummyData from "../../dummy-data"
 const PostsPage = () => {
   // set up state for your data
+  const [instaOn, setinstaOn ] = useState(dummyData)
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+
+     instaOn.map(props => {
+       return (
+         <Post post={props}/>
+       )
+     } )
+      }
     </div>
   );
 };
